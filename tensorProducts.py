fdf46c5f0abb185d09d1bdc17d97a20b32726955
Lara <laratatli@hotmail.com>
2024-10-27 09:06:10 +0000

    Updated calculations

--- tensorProducts.py
+++ tensorProducts.py
@@ -1,14 +1,65 @@
 import numpy as np
 import random
-# Pauli-Z Matrix
+
 sigma_z = np.array([[1, 0],
                     [0, -1]])
 
+sigma_x = np.array([[0, 1],
+                    [1, 0]])
+
 I_2 = np.eye(2)
 
-tensor_product = np.kron(sigma_z, I_2)
-print(tensor_product)
+# define a function which calculates tensor product across n qubits, performing operation on kth qubit
+# determine problem hamiltonian by encoding maximum independent set problem in Ising model
+# apply time evolution operator
+# plot probability of success as a function of t/tmax, where tmax is the total runtime of the algorithm
 
+# Calculating sigma^z_2:
+
+intermediate = np.kron(I_2, sigma_z)
+sigma_z_2 = np.kron(intermediate, I_2)
+#print(sigma_z_2)
+
+n = 3
+j = 2
+
+def sigma_z_j(n,j):
+    " Tensor product for sigma^z_j, where n is the total number of qubits in the system and the Pauli-Z gate is applied to the jth qubit "
+    calc = np.eye(1) # Intialise calculation, apply identity to the first (j-1) qubits
+
+    for i in range(1, j):
+        calc = np.kron(calc, I_2)
+
+    # Apply σ^z at the j-th position
+    calc = np.kron(calc, sigma_z)
+
+    # Tensor product of (n-j) identity matrices from (j+1) to nth qubit
+    for i in range(j + 1, n + 1):
+        calc = np.kron(calc, I_2)
+
+    calc[np.isclose(calc, 0)] = 0 # Changes all instances of -0. to 0.
+    return calc
+
+
+def sigma_x_j(n,j):
+    " Tensor product for sigma^x_j, where n is the total number of qubits in the system and the Pauli-X gate is applied to the jth qubit "
+    calc = np.eye(1) # Intialise calculation, apply identity to the first (j-1) qubits
+
+    for i in range(1, j):
+        calc = np.kron(calc, I_2)
+
+    # Apply σ^x at the j-th position
+    calc = np.kron(calc, sigma_x)
+
+    # Tensor product of (n-j) identity matrices from (j+1) to nth qubit
+    for i in range(j + 1, n + 1):
+        calc = np.kron(calc, I_2)
+
+    calc[np.isclose(calc, 0)] = 0 # Changes all instances of -0. to 0.
+    return calc
+
+
+    
 
 
 
